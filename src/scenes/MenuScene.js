19b476bc7cd60c3c756223ea7c426c29b777b2d4
simon-pelletier <symasp@gmail.com>
2022-05-28 11:51:05 +0200

    Menu scene and global config to scale fullscreen

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -4,21 +4,21 @@
 /*                                                                            */
 /* ========================================================================== */
 
-import * as Helpers from '../helpers/helpers';
-import * as Setup from '../setup';
+import * as Helpers from "../helpers/helpers";
+import * as Setup from "../setup";
 
-import defaultSceneConfig from '../helpers/sceneConfig';
+import defaultSceneConfig from "../helpers/sceneConfig";
 
-import generateUnivers from '../helpers/univerSizer';
+import generateUnivers from "../helpers/univerSizer";
 
-import Player from '../objects/Player';
+import Player from "../objects/Player";
 
-import Game from '../game';
+import Game from "../game";
 
 class MenuScene extends Phaser.Scene {
-    constructor(test) {
+    constructor() {
         super({
-            key: 'MenuScene'
+            key: "MenuScene",
         });
     }
 
@@ -27,7 +27,6 @@ class MenuScene extends Phaser.Scene {
     /* ========================================================================== */
 
     create() {
-
         /* ------------------------------ Scene config ------------------------------ */
 
         // Variables Globales
@@ -39,7 +38,7 @@ class MenuScene extends Phaser.Scene {
         // Ajoute au curseur la fonction Drag&Drop
         this.matter.add.mouseSpring({
             length: 1,
-            stiffness: 0.6
+            stiffness: 0.6,
         });
 
         // Définit les bords de la scene en colliders
@@ -48,28 +47,40 @@ class MenuScene extends Phaser.Scene {
         /* ----------------------------- MATTER OBJECTS ----------------------------- */
 
         // Ajoute le logo du jeu
-        this.logo = this.matter.add.image(Setup.ORIGIN_X, Setup.ORIGIN_Y - 60, 'logo', null, {
-            chamfer: 16,
-            ignoreGravity: true
-        })
+        this.logo = this.matter.add.image(
+            Setup.ORIGIN_X,
+            Setup.ORIGIN_Y - 60,
+            "logo",
+            null,
+            {
+                chamfer: 16,
+                ignoreGravity: true,
+            }
+        );
 
         // Ajoute le bouton 'Play'
-        this.playBtn = this.matter.add.sprite(Setup.ORIGIN_X, Setup.ORIGIN_Y + 130, 'play', null, {
-
-
-            ignoreGravity: true
-        }).setInteractive({
-            cursor: 'url(./assets/cursor/select.cur), pointer'
-        });
+        this.playBtn = this.matter.add
+            .sprite(Setup.ORIGIN_X, Setup.ORIGIN_Y + 130, "play", null, {
+                ignoreGravity: true,
+            })
+            .setInteractive({
+                cursor: "url(./assets/cursor/select.cur), pointer",
+            });
 
         // Ajoute le this.shipMenu
-        this.shipMenu = this.matter.add.sprite(Setup.WIDTH - 100, Setup.ORIGIN_Y + 150, 'ship', null, {
-            isStatic: false,
-            shape: {
-                type: 'circle',
-                radius: 120
+        this.shipMenu = this.matter.add.sprite(
+            Setup.WIDTH - 100,
+            Setup.ORIGIN_Y + 150,
+            "ship",
+            null,
+            {
+                isStatic: false,
+                shape: {
+                    type: "circle",
+                    radius: 120,
+                },
             }
-        });
+        );
         this.shipMenu.setScale(0.2);
         this.shipMenu.setFrictionAir(0.01);
         this.shipMenu.setMass(3);
@@ -78,58 +89,77 @@ class MenuScene extends Phaser.Scene {
         this.shipMenu.setAngle(-90);
 
         /* ------------------------------- ANIMATIONS ------------------------------- */
-        this.shipMenu.anims.play('idleShip');
+        this.shipMenu.anims.play("idleShip");
 
         /* --------------------------------- GROUPS --------------------------------- */
 
         // Ajoute le groupe Background
         this.backGround = this.add.group({
-            key: 'starOnePx',
-            frameQuantity: 150
+            key: "starOnePx",
+            frameQuantity: 150,
         });
 
+        console.log("Phaser", Phaser);
         // Créé les étoiles du background
-        this.starBackground = new Phaser.Geom.Rectangle(0, 0, Setup.WIDTH, Setup.HEIGHT);
-        Phaser.Actions.RandomRectangle(this.backGround.getChildren(), this.starBackground);
+        this.starBackground = new Phaser.Geom.Rectangle(
+            0,
+            0,
+            window.innerWidth,
+            window.innerHeight
+        );
+
+        Phaser.Actions.RandomRectangle(
+            self.backGround.getChildren(),
+            self.starBackground
+        );
+
+        function resize() {
+            self.starBackground.width = window.innerWidth;
+            self.starBackground.height = window.innerHeight;
+            Phaser.Actions.RandomRectangle(
+                self.backGround.getChildren(),
+                self.starBackground
+            );
+        }
+        window.onresize = resize;
 
         /* --------------------------------- SOUNDS --------------------------------- */
 
         // Ajoute le son de musique d'intro et le joue en boucle
-        this.musicIntro = this.sound.add('intro');
+        this.musicIntro = this.sound.add("intro");
         this.musicIntro.volume = 0.5;
         this.musicIntro.loop = true;
         //this.musicIntro.play();
 
         // Ajout du son de Thrusters
-        this.soundThrusterTop = this.sound.add('soundThruster');
+        this.soundThrusterTop = this.sound.add("soundThruster");
         this.soundThrusterTop.volume = 0.2;
         this.soundThrusterTop.loop = true;
 
         /* -------------------------------- LISTENERS ------------------------------- */
 
         // Anime les états du bouton 'PLAY'
-        this.playBtn.on('pointerover', function () {
+        this.playBtn.on("pointerover", function () {
             this.setScale(1.1, 1.1);
             this.setTint(0xdc324a);
 
-            self.shipMenu.anims.play('upAnimShip');
+            self.shipMenu.anims.play("upAnimShip");
             self.soundThrusterTop.play();
             self.shipMenu.thrustLeft(0.1);
             //self.shipMenu.setVelocityX(Helpers.getRandomNumber(-5, -10));
             //self.shipMenu.setAngularVelocity(Helpers.getRandomNumberFloat(0.03, 0.1));
         });
-        this.playBtn.on('pointerout', function () {
+        this.playBtn.on("pointerout", function () {
             this.setScale(1, 1);
             this.setTint(0xcccccc);
 
-            self.shipMenu.anims.play('idleShip');
+            self.shipMenu.anims.play("idleShip");
             self.soundThrusterTop.stop();
         });
-        this.playBtn.on('pointerdown', function () {
+        this.playBtn.on("pointerdown", function () {
             this.setTint(0xcccccc);
         });
-        this.playBtn.on('pointerup', this.doStart.bind(this));
-
+        this.playBtn.on("pointerup", this.doStart.bind(this));
     }
 
     /* ========================================================================== */
@@ -137,7 +167,6 @@ class MenuScene extends Phaser.Scene {
     /* ========================================================================== */
 
     doStart() {
-
         // Arrete le son de thruster
         this.soundThrusterTop.stop();
 
@@ -151,8 +180,8 @@ class MenuScene extends Phaser.Scene {
         // Object Planet
         this.player = new Player({
             scene: this,
-            name: 'playerNameTemp',
-            key: 'player'
+            name: "playerNameTemp",
+            key: "player",
         });
 
         Game.player = this.player;
@@ -162,10 +191,9 @@ class MenuScene extends Phaser.Scene {
         this.musicIntro.stop();
 
         // Démarre la scene Planet (début de partie)
-        this.scene.start(Setup.STARTSCENE + 'Scene');
+        this.scene.start(Setup.STARTSCENE + "Scene");
 
-        this.scene.launch('UiScene');
-
+        this.scene.launch("UiScene");
     }
 }
 
