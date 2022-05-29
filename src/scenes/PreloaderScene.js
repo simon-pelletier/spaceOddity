0e7de8a295b24cf08626e921b17873617038e601
simon-pelletier <symasp@gmail.com>
2022-05-29 07:59:54 +0200

    Cleaning comments & adapt preloader resize

--- src/scenes/PreloaderScene.js
+++ src/scenes/PreloaderScene.js
@@ -1,35 +1,32 @@
-/* ========================================================================== */
-/*                                                                            */
-/*                              PRELOADER - SCENE                             */
-/*                                                                            */
-/* ========================================================================== */
-
 import * as Setup from '../setup';
+import Game from '../game';
 
 import shipAnimations from '../animations/shipAnimations';
 import geyserAnimations from '../animations/geyserAnimations';
 import rawMatAnimations from '../animations/rawMatAnimations';
 
 class PreloaderScene extends Phaser.Scene {
-    constructor(test) {
+    constructor() {
         super({
             key: 'PreloaderScene',
             pack: {
-                files: [{
-                    type: 'image',
-                    key: 'logo',
-                    url: './assets/img/ui/logo.png'
-                }]
+                files: [
+                    {
+                        type: 'image',
+                        key: 'logo',
+                        url: './assets/img/ui/logo.png'
+                    }
+                ]
             }
         });
     }
 
-    /* ========================================================================== */
-    /*                             SET PRELOAD SPRITE                             */
-    /* ========================================================================== */
-
+    /**
+     * Sprite preloader
+     * @param {object} sprite
+     */
     setPreloadSprite(sprite) {
-        // Configure l'image de chargement
+        // Configure the loading image
         this.preloadSprite = {
             sprite: sprite,
             width: sprite.width,
@@ -37,38 +34,30 @@ class PreloaderScene extends Phaser.Scene {
         };
         sprite.visible = true;
 
-        // Définit un callBack pour onProgress
+        // Define a callBack for onProgress
         this.load.on('progress', this.onProgress, this);
-        //this.load.on('fileprogress', this.onFileProgress, this);
     }
 
-    /* ========================================================================== */
-    /*                                 ON PROGRESS                                */
-    /* ========================================================================== */
-
+    /**
+     * On progress loading
+     * @param {number} value
+     */
     onProgress(value) {
         if (this.preloadSprite) {
-            // Calcule la hauteur (0.0 - 1.0)
+            // Calculate height (0.0 - 1.0)
             let h = Math.floor(this.preloadSprite.height * value);
 
-            // Hauteur du Sprite		
+            // Sprite height
             this.preloadSprite.sprite.frame.height = h;
             this.preloadSprite.sprite.frame.cutHeight = h;
 
-            // Mets à jour l'image
+            // Image update
             this.preloadSprite.sprite.frame.updateUVs();
         }
     }
 
-    /* ========================================================================== */
-    /*                                   PRELOAD                                  */
-    /* ========================================================================== */
-
     preload() {
-
-        /* --------------------------- Fichiers à charger --------------------------- */
-
-        /* --------------------------------- IMAGES --------------------------------- */
+        //* IMAGES
 
         this.load.image('logo', './assets/img/ui/logo.png');
         this.load.image('play', './assets/img/ui/play.png');
@@ -85,13 +74,13 @@ class PreloaderScene extends Phaser.Scene {
         this.load.image('smokeWhite', './assets/img/smokeWhite.png');
         this.load.image('fire', './assets/img/fire.png');
 
-        // Ui
+        // Ui parts
         this.load.image('needle', './assets/img/ui/needle.png');
         this.load.image('needleH', './assets/img/ui/needleH.png');
         this.load.image('ui_base', './assets/img/ui/ui_base.png');
         this.load.image('hscBulb', './assets/img/ui/hscBulb.png');
 
-        /* ------------------------------- SRITESHEETS ------------------------------ */
+        //* SRITESHEETS
 
         this.load.spritesheet('ship', './assets/img/sprites/shipAnim.png', {
             frameWidth: 300,
@@ -106,52 +95,91 @@ class PreloaderScene extends Phaser.Scene {
             frameHeight: 100
         });
 
-        /* --------------------------------- SOUNDS --------------------------------- */
+        //* SOUNDS
 
-        this.load.audio('intro', ['./assets/snd/intro.mp3', './assets/snd/intro.vaw']);
-        this.load.audio('soundThruster', ['./assets/snd/thrust.mp3', './assets/snd/thrust.vaw']);
-        this.load.audio('impactShipNormal', ['./assets/snd/impactShipNormal.mp3', './assets/snd/impactShipNormal.vaw']);
-        this.load.audio('pump', ['./assets/snd/pump.mp3', './assets/snd/pump.vaw']);
-        this.load.audio('airSteam', ['./assets/snd/airSteam.mp3', './assets/snd/airSteam.vaw']);
-        this.load.audio('robotMove', ['./assets/snd/robotMove.mp3', './assets/snd/robotMove.vaw']);
-        this.load.audio('welding', ['./assets/snd/welding.mp3', './assets/snd/welding.vaw']);
-        this.load.audio('drill', ['./assets/snd/drill.mp3', './assets/snd/drill.vaw']);
-        
-        // Fonts
-        this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
+        this.load.audio('intro', [
+            './assets/snd/intro.mp3',
+            './assets/snd/intro.vaw'
+        ]);
+        this.load.audio('soundThruster', [
+            './assets/snd/thrust.mp3',
+            './assets/snd/thrust.vaw'
+        ]);
+        this.load.audio('impactShipNormal', [
+            './assets/snd/impactShipNormal.mp3',
+            './assets/snd/impactShipNormal.vaw'
+        ]);
+        this.load.audio('pump', [
+            './assets/snd/pump.mp3',
+            './assets/snd/pump.vaw'
+        ]);
+        this.load.audio('airSteam', [
+            './assets/snd/airSteam.mp3',
+            './assets/snd/airSteam.vaw'
+        ]);
+        this.load.audio('robotMove', [
+            './assets/snd/robotMove.mp3',
+            './assets/snd/robotMove.vaw'
+        ]);
+        this.load.audio('welding', [
+            './assets/snd/welding.mp3',
+            './assets/snd/welding.vaw'
+        ]);
+        this.load.audio('drill', [
+            './assets/snd/drill.mp3',
+            './assets/snd/drill.vaw'
+        ]);
 
-        /* -------------------------------- Preloader ------------------------------- */
+        //* FONTS
 
-        // Affiche la barre de chargement graphique
-        this.loadingbar_bg = this.add.sprite(Setup.ORIGIN_X, Setup.ORIGIN_Y - 60, "logo").setTint(0xdc324a);
-        this.loadingbar_fill = this.add.sprite(Setup.ORIGIN_X, Setup.ORIGIN_Y - 60, "logo").setTint(0xdc324a);
+        this.load.script(
+            'webfont',
+            'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
+        );
+
+        //* PRELOADER
+
+        // Displays the graphical loading bar
+        this.loadingbar_bg = this.add
+            .sprite(
+                Game.canvas.clientWidth / 2,
+                Game.canvas.clientHeight / 2 - 60,
+                'logo'
+            )
+            .setTint(0xdc324a);
+        this.loadingbar_fill = this.add
+            .sprite(
+                Game.canvas.clientWidth / 2,
+                Game.canvas.clientHeight / 2 - 60,
+                'logo'
+            )
+            .setTint(0xdc324a);
         this.setPreloadSprite(this.loadingbar_fill);
 
-        // Pour voir le preloader graphiquement (actif en devMod)
-        if (Setup.DEVMOD == true) {
+        // To see the preloader graphically (active in devMod)
+        if (Setup.DEVMOD === true) {
             for (var i = 0; i < 100; i++) {
                 this.load.image('testloading' + i, './assets/img/ui/logo.png');
-            };
+            }
         }
-
-
     }
 
-    /* ========================================================================== */
-    /*                                   CREATE                                   */
-    /* ========================================================================== */
-
     create() {
-        // Charge les animations du Ship
+        // Load Ship animations
         shipAnimations(this);
-        // Charge les animations de Geyser
+        // Loads Geyser Animations
         geyserAnimations(this);
-        // Charge les animations de RawMat
+        // Load RawMat animations
         rawMatAnimations(this);
-        // Vide le preload
+        // Empty the preload
         this.preloadSprite = null;
-        // Démarre la scene de départ
-        this.scene.start('MenuScene');
+        // Start the starting scene or the Dev Scene if DevMod
+        if (Setup.DEVMOD === true) {
+            console.log('Setup.DEVSCENE', Setup.DEVSCENE);
+            this.scene.start(Setup.DEVSCENE);
+        } else {
+            this.scene.start('MenuScene');
+        }
     }
 }
 
